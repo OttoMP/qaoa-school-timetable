6b079caf46053235030c1d5bf85d6fdbd6002444
OttoMP <otto28mai@gmail.com>
2021-08-19 09:12:13 -0300

    memory usage analysis

--- test.py
+++ test.py
@@ -9,38 +9,50 @@ import numpy as np
 import networkx as nx
 
 # Import miscellaneous tools
-from pympler import asizeof, muppy, summary, tracker, refbrowser
-import os, psutil
-from guppy import hpy
-import objgraph
-import datetime
 import gc
+import os
+import psutil
+from pympler import asizeof, muppy, summary, tracker, refbrowser
 import sys
 from xml_parser import parseXML
 from itertools import combinations
 import pprint as pp
 
-def dump_heap(h, i):
-   '''
-   @param h: The heap (from hp = hpy(), h = hp.heap())
-   @param i: Identifier str
-   '''
-  
-   print("Dumping stats at: {0}".format(i))
-   print("Memory usage: {0} (MB)".format(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2))
-   print("Most common types:")
-   objgraph.show_most_common_types()
-  
-   print("heap is:")
-   print("{0}".format(h))
-  
-   #by_refs = h.byrcs
-   #print("by references: {0}".format(by_refs))
-  
-   #print("More stats for top element..")
-   #print("By clodo (class or dict owner): {0}".format(by_refs[0].byclodo))
-   #print("By size: {0}".format(by_refs[0].bysize))
-   #print("By id: {0}".format(by_refs[0].byid))
+def cost_function_timetable(G, num_colors, list_students):
+    C = 0
+    lectures = G.nodes
+    for student in list_students:
+        lecture_student = [(j,k,l) for (j,k,l) in lectures if l == student]
+        timeslots = [G.nodes[key]['color'] for key in list(lecture_student)]
+        timeslots.sort()
+        new_timeslots = [x%num_colors for x in timeslots]
+
+        # Students shouldn't have lectures at the last time of the day
+        for time in new_timeslots:
+            if time % num_colors == num_colors-1:
+                C += 1
+
+        day = []
+        last_lecture = -np.inf
+        for time in new_timeslots:
+            if last_lecture >= time:
+                # Students shouldn't have only one lecture in a day
+                if len(day) == 1:
+                    C += 1
+                #Students shouldn't have many consecutive lectures
+                else:
+                    for index, lect in enumerate(day):
+                        if index+1 < len(day) and day[index+1] == lect+1:
+                            C += 1
+                day = []
+
+            last_lecture = time
+            day.append(last_lecture)
+        for index, lect in enumerate(day):
+            if index+1 < len(day) and day[index+1] == lect+1:
+                C += 1
+
+    return C
 
 def cost_function_den_25pts(G):
     C = 0
@@ -239,11 +251,15 @@ def phase_separator(qc, gamma, num_nodes, num_colors):
         X(qc[node])
     #color2qubits(qc, num_nodes, num_colors)
 
-def qaoa_min_graph_coloring(p, G, num_nodes, num_colors, beta0, gamma, beta):
+def qaoa_min_graph_coloring(p, G, num_nodes, num_colors, beta0, gamma, beta, tr):
+    #print("Memory Usage start result", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    #tr.print_diff() 
     # --------------------------
     # Initializing qubits
     # --------------------------
     qc = quant((num_nodes*num_colors) + num_nodes)
+    #print("Memory Usage after initi qubit", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    #tr.print_diff() 
 
     # --------------------------
     # Initial state preparation
@@ -251,6 +267,8 @@ def qaoa_min_graph_coloring(p, G, num_nodes, num_colors, beta0, gamma, beta):
     coloring = [G.nodes[node]['color'] for node in G.nodes]
     for i, color in enumerate(coloring):
         X(qc[(i*num_colors)+color])
+    #print("Memory Usage after state prep", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    #tr.print_diff() 
 
     # --------------------------
     # Alternate application of operators
@@ -260,6 +278,8 @@ def qaoa_min_graph_coloring(p, G, num_nodes, num_colors, beta0, gamma, beta):
         phase_separator(qc, gamma[step], num_nodes, num_colors)
         #phase_separator_ad_hoc(qc, gamma[step], num_nodes, num_colors)
         mixer(qc, G, beta[step], num_nodes, num_colors)
+    #print("Memory Usage after operators", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    #tr.print_diff() 
 
     # --------------------------
     # Measurement
@@ -267,20 +287,9 @@ def qaoa_min_graph_coloring(p, G, num_nodes, num_colors, beta0, gamma, beta):
     #result = measure(qc).get()
     return dump(qc)
 
-def counting_states(result, num_nodes, num_colors):
-    counts = {} # Dictionary for keeping the results of the simulation
-    for i in result.get_states():
-        binary = np.binary_repr(i, width=(num_nodes*num_colors)+num_nodes)
-        counts[binary] = int(2**20*result.probability(i))
-
-    return counts
-
-def output_function(o):
-    return str(type(o))
-
-def qaoa(par, p, tr):
-    print("Memory Usage start qaoa call", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
-    tr.print_diff() 
+def qaoa(par, p, G, num_colors, students_list, tr):
+    #print("Memory Usage start qaoa call", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    #tr.print_diff() 
     # --------------------------
     # Unpacking QAOA parameters
     # --------------------------
@@ -289,32 +298,22 @@ def qaoa(par, p, tr):
     gamma = par[1:middle+1]
     beta = par[middle+1:]
 
-    print("Memory Usage after parameters", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
-    tr.print_diff() 
+    #print("Memory Usage after parameters", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    #tr.print_diff() 
     # --------------------------
     # Verifying Parameters
     # --------------------------
     #print("Using Following parameters:\nBeta0:", beta0, "\nGamma:", gamma, "\nBeta:", beta)
 
+    num_nodes = G.number_of_nodes()
     # --------------------------
     # Running QAOA on simulator
     # --------------------------
-
-    #G = nx.Graph()
-    #G.add_nodes_from(initial_G)
-    #G.add_edges_from(initial_G.edges)
-    #initial_coloring = [initial_G.nodes[node]['color'] for node in initial_G.nodes]
-    #color_graph_from_coloring(G, initial_coloring)
-    G, num_colors = create_full_graph(tr)
-    num_nodes = G.number_of_nodes()
-    number_of_qubits = num_nodes*num_colors+num_nodes
-    #print("Necessary number of qubits: ", number_of_qubits)
-    print("Memory Usage after graph", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
-    tr.print_diff() 
-    
-    result = qaoa_min_graph_coloring(p, G, num_nodes, num_colors, beta0, gamma, beta)
-    print("Memory Usage after result", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
-    tr.print_diff() 
+    #print("Memory Usage before result", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    #tr.print_diff() 
+    result = qaoa_min_graph_coloring(p, G, num_nodes, num_colors, beta0, gamma, beta, tr)
+    #print("Memory Usage after result", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    #tr.print_diff() 
     
     #print("Number of States", len(result.get_states()))
     #print("State Vector", result.show('b6:b6:b6:b6:b6:b6'))
@@ -323,12 +322,12 @@ def qaoa(par, p, tr):
     # Counting resulting states
     # --------------------------
     counts = {} # Dictionary for keeping the results of the simulation
-    for i in result.get_states():
-        binary = np.binary_repr(i, width=(num_nodes*num_colors)+num_nodes)
+    for i in result.states:
+        binary = f'{i:0{(num_nodes*num_colors)+num_nodes}b}'
         counts[binary] = int(2**20*result.probability(i))
     #counts = counting_states(result, num_nodes, num_colors)
-    print("Memory Usage after get states", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
-    tr.print_diff() 
+    #print("Memory Usage after get states", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    #tr.print_diff() 
     # --------------------------
     # Evaluate the data from the simulator
     # --------------------------
@@ -348,7 +347,8 @@ def qaoa(par, p, tr):
                         coloring.append(pos)
             color_graph_from_coloring(G, coloring)
 
-            fx = cost_function_den_25pts(G)
+            fx = cost_function_timetable(G, num_colors, students_list)
+            #fx = cost_function_den_25pts(G)
             #fx = cost_function_den_4pts(G)
 
             # compute the expectation value and energy distribution
@@ -358,17 +358,16 @@ def qaoa(par, p, tr):
             if( min_function_value[1] > fx):
                 min_function_value[0] = sample
                 min_function_value[1] = fx
+    
+    #print("Memory Usage after counts", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    #tr.print_diff() 
 
-    print("Memory Usage after counts", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
-    tr.print_diff() 
-
-    #expected_value = avr_function_value/sum(counts.values())
+    expected_value = avr_function_value/sum(counts.values())
     # Return expected value
-    #cb_result = refbrowser.ConsoleBrowser(result, maxdepth=2, str_func=output_function)
-    #cb_result.print_tree() 
-    print("Memory Usage end qaoa call", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
-    tr.print_diff() 
-    return avr_function_value/sum(counts.values())
+    #print("Memory Usage end qaoa call", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    #tr.print_diff()
+    
+    return expected_value
 
 def color_graph_greedy(G):
     pair = None, G.number_of_nodes(), 0
@@ -488,29 +487,42 @@ def create_graph_from_events(events):
             G.add_edge(i[0]['Id'], i[1]['Id'])
     return G
 
+def create_graph_from_list(nodes, edges):
+    G = nx.Graph()
+    G.add_nodes_from([(tuple, {'color' : None}) for tuple in nodes])
+
+    for e, row in enumerate(edges):
+        for f, column in enumerate(row):
+            if column == 1:
+               G.add_edge(nodes[e],nodes[f])
+
+    return G
+
 def create_full_graph(tr):
-    print("Memory Usage start graph", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
-    tr.print_diff() 
+    #print("Memory Usage start graph", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    #tr.print_diff() 
     # --------------------------
     # Parse XML file
     # --------------------------
     events = parseXML('dataset/den-smallschool.xml')
-    print("Memory Usage after parse", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
-    tr.print_diff() 
+    #print("Memory Usage after parse", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    #tr.print_diff() 
 
     # --------------------------
     #  Preparing Conflict Graph
     # --------------------------
     G = create_graph_from_events(events)
+    #print("Memory Usage after creating graph", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    #tr.print_diff() 
     
     #print("--------------------------")
     #print("Graph information\n")
     
     #print("Nodes = ", G.nodes)
-    coloring = [G.nodes[node]['color'] for node in G.nodes]
+    #coloring = [G.nodes[node]['color'] for node in G.nodes]
     #print("\nPre-coloring", coloring)
 
-    degree = [deg for (node, deg) in G.degree()]
+    #degree = [deg for (node, deg) in G.degree()]
     #print("\nDegree of each node", degree)
 
     # --------------------------
@@ -527,8 +539,10 @@ def create_full_graph(tr):
     # -----------------------------------------------------------------
     num_colors = 5 # Denmark colors
     color_graph_from_num(G, num_colors)
+    #print("Memory Usage after coloring graph", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    #tr.print_diff() 
     
-    coloring = [G.nodes[node]['color'] for node in G.nodes]
+    #coloring = [G.nodes[node]['color'] for node in G.nodes]
     #coloring =  [1, 0, 2, 3, 1, 2, 1, 2, 3, 0, 0, 2, 0, 3, 1, 3, 0, 1, 0, 3, 2, 2, 1, 2, 3]
     #coloring =  [0, 2, 3, 1, 2, 3, 3, 2, 0, 1, 0, 3, 2, 1, 0, 2, 3, 0, 2, 1, 3, 3, 0, 3, 1]
     #color_graph_from_coloring(G, coloring)
@@ -537,9 +551,9 @@ def create_full_graph(tr):
     #print("\nNumber of colors", num_colors)
     #print("\nInitial coloring", coloring)
 
-    initial_function_value = cost_function_den_25pts(G)
+    #initial_function_value = cost_function_den_25pts(G)
     #print("\nInitial Function Value Max 25", initial_function_value)
-    initial_function_value = cost_function_den_4pts(G)
+    #initial_function_value = cost_function_den_4pts(G)
     #print("\nInitial Function Value Max 4", initial_function_value)
 
     # ---------------------------
@@ -553,9 +567,86 @@ def create_full_graph(tr):
         #print("Neighbours | Color")
         #for pair in color_and_neighbour:
             #print(pair)
-    
     return G, num_colors
  
+def first_example():
+    # Problem variables
+    num_weeks = 1
+    num_days = 1 #5
+    num_periods = 6
+    num_timeslots = num_days*num_periods
+
+    # Each subject has only one teacher
+    # Each teacher teaches only one subject
+    num_subjects = 3
+    num_teachers = num_subjects
+    num_students = 2
+    num_rooms = 3
+
+    # Number of features in a room
+    # Ex.: has computers, has >40 chairs...
+    num_features = 2
+
+    teachers_list = [teacher for teacher in range(num_teachers)]
+    students_list = [student for student in range(num_students)]
+
+    #roomFeatures = np.matrix([[0 for feature in range(num_rooms)] for event in range(num_features)])
+    roomFeatures = np.matrix([[0, 1, 1],
+                             [1, 0, 0]])
+    #subjectFeatures = np.matrix([[0 for feature in range(num_features)] for event in range(num_subjects)])
+    subjectFeatures = np.matrix([[1, 0],
+                                [1, 0],
+                                [0, 1]])
+    suitableRoom = subjectFeatures*roomFeatures
+
+    # Allocate rooms
+    # Each subject will be allocated to the least busy room
+    allocations = []
+    num_allocations = [0 for room in range(num_rooms)]
+
+    for subject_index, subject in enumerate(suitableRoom.tolist()):
+        possible_allocations = []
+        for index, room in enumerate(subject):
+            if room == 1:
+                possible_allocations.append(index)
+
+        min_allocations = np.inf
+        allocated_room = np.inf
+        for alloc_index in possible_allocations:
+            if num_allocations[alloc_index] < min_allocations:
+                allocated_room = alloc_index
+                min_allocations = num_allocations[allocated_room]
+        allocations.append((subject_index, allocated_room))
+        num_allocations[allocated_room] += 1
+
+    # Pair subjects with students
+    # lecture = (subject, room, student)
+    lectures = [(j,k,l) for j,k in allocations for l in students_list]
+
+    # Generate the lectureConflict Matrix
+    # The hard constraints of the problem are included in the matrix
+    lectureConflict = [[0 for feature in range(len(lectures))] for event in range(len(lectures))]
+
+    # If two lectures are allocated to the same room,
+    # share a student or have the same teacher they
+    # cannot be assigned to the same timeslot
+    for e, j in enumerate(lectures):
+        subject,room,student = j
+        for f,a in enumerate(lectures[e+1:]):
+            subject2,room2,student2 = a
+            if subject == subject2:
+                lectureConflict[e][e+1+f] = 1
+                lectureConflict[e+1+f][e] = 1
+            if student == student2:
+                lectureConflict[e][e+1+f] = 1
+                lectureConflict[e+1+f][e] = 1
+            if room == room2:
+                lectureConflict[e][e+1+f] = 1
+                lectureConflict[e+1+f][e] = 1
+
+    G = create_graph_from_list(lectures, lectureConflict)
+
+    return G, students_list
 
 def main():
     print("Starting program\n")
@@ -564,11 +655,26 @@ def main():
     # --------------------------
     # School Instances
     # --------------------------
-    school = "Den"
+    school = "CEC"
            
     #----------------------------
     # Starting QAOA
     #----------------------------
+    initial_G, students_list = first_example()
+    #initial_G, num_colors = create_full_graph(tr)
+    
+    num_colors = 6        # CEC example colors
+    color_graph_from_coloring(initial_G, [0,3,1,4,2,5])
+    G = nx.Graph()
+    G.add_nodes_from(initial_G)
+    G.add_edges_from(initial_G.edges)
+    initial_coloring = [initial_G.nodes[node]['color'] for node in initial_G.nodes]
+    color_graph_from_coloring(G, initial_coloring)
+    
+    num_nodes = G.number_of_nodes()
+    number_of_qubits = num_nodes*num_colors+num_nodes
+    print("Necessary number of qubits: ", number_of_qubits)
+
     print("----------------------------")
     print("Running QAOA")
     # QAOA parameter
@@ -579,16 +685,24 @@ def main():
     beta  = [random.uniform(0, np.pi) for _ in range(p)]
     s = [beta0]+gamma+beta
     tr.print_diff() 
-    #for i in range(1):
-    print("Memory Usage before qaoa call", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    for i in range(10):
+        print("Iteration:", i)
+        print("Memory Usage before qaoa call", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+        print("Muppy all objects", len(muppy.get_objects()))
+        tr.print_diff()
+        print("Garbage counter", len(gc.garbage))
+        print()
+        #ev = qaoa(s, p, G, num_colors, tr)
+        ev = qaoa(s, p, G, num_colors, students_list, tr)
+        print("Memory Usage after qaoa call", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+        print("Muppy all objects", len(muppy.get_objects()))
+        tr.print_diff() 
+        print("Garbage counter", len(gc.garbage))
+        print("-------------------")
+    print("Memory Usage after gc", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
     print("Muppy all objects", len(muppy.get_objects()))
     tr.print_diff() 
-    ev = qaoa(s, p, tr)
-    print("Memory Usage after qaoa call", psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
-    print("Muppy all objects", len(muppy.get_objects()))
-    tr.print_diff() 
-    
-    print("Program End")
+    print("Garbage counter", len(gc.get_objects()))
 
 if __name__ == '__main__':
     main()
