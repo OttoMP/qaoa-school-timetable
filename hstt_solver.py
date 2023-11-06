7221b94be20344a4c7030f5c50e33294315e35c6
Evandro Chagas Ribeiro da Rosa <evandro@quantuloop.com>
2023-11-06 15:49:40 -0300

    refac qubo converter

--- hstt_solver.py
+++ hstt_solver.py
@@ -1,10 +1,13 @@
 import os, sys
 import neal
-#from dwave.samplers import DWaveSampler
+
+# from dwave.samplers import DWaveSampler
 from xhsttparser import XHSTT
 from qubo import xhstt_to_qubo
 from pprint import pprint
-from qat.core import Observable, Term
+
+# from qat.core import Observable, Term
+
 
 def main():
     print("Starting program\n")
@@ -13,22 +16,22 @@ def main():
     # --------------------------
     # School Instances
     # --------------------------
-    #path = os.path.abspath(os.path.join(os.getcwd(), 'dataset', sys.argv[1]+".xml"))
+    # path = os.path.abspath(os.path.join(os.getcwd(), 'dataset', sys.argv[1]+".xml"))
     path = sys.argv[1]
 
     # --------------------------
     # Parse XML file
     # --------------------------
-    #events = parseXML('dataset/den-smallschool.xml')
+    # events = parseXML('dataset/den-smallschool.xml')
     xhstt = XHSTT(path)
 
     instance = next(iter(xhstt.instances.values()))
-    
-    #print(instance)
-    #print(instance.times, end="\n\n")
-    #print(instance.resources, end="\n\n")
-    #print(instance.events, end="\n\n")
-    #print(instance.constraints)
+
+    # print(instance)
+    # print(instance.times, end="\n\n")
+    # print(instance.resources, end="\n\n")
+    # print(instance.events, end="\n\n")
+    # print(instance.constraints)
 
     # ------------------------------
     #  Preparing Problem Formulation
@@ -36,38 +39,37 @@ def main():
 
     model = xhstt_to_qubo(instance)
     qubo, offset = model.to_qubo()
-    #pprint(qubo)
-    #pprint(offset)
+    # pprint(qubo)
+    # pprint(offset)
 
-    #----------------------------
+    # ----------------------------
     # Starting QAOA
-    #----------------------------
-    print("----------------------------")
-    print("Running QAOA")
-    number_of_qubits = len(model.variables)
-    print(f"Necessary number of qubits: {number_of_qubits}")
+    # ----------------------------
+    # print("----------------------------")
+    # print("Running QAOA")
+    # number_of_qubits = len(model.variables)
+    # print(f"Necessary number of qubits: {number_of_qubits}")
     # QAOA parameter
-    final_depth = 8
-    #print(f"Final value of p reached: {final_depth}")
+    # final_depth = 8
+    # print(f"Final value of p reached: {final_depth}")
 
-    single_values = {}
-    multiple_values = {}
-    indexes = model.variables[::-1]
-    for k,v in qubo.items():
-        if k[0] == k[1]:
-            single_values[indexes.index(k[0])] = v
-        else:
-            multiple_values[indexes.index(k[0]), indexes.index(k[1])] = v
+    # single_values = {}
+    # multiple_values = {}
+    # indexes = model.variables[::-1]
+    # for k, v in qubo.items():
+    #    if k[0] == k[1]:
+    #        single_values[indexes.index(k[0])] = v
+    #    else:
+    #        multiple_values[indexes.index(k[0]), indexes.index(k[1])] = v
+    #
+    # hamiltonian_qubo = Observable(
+    #    number_of_qubits,
+    #    pauli_terms=[Term(single_values[x], "Z", [x]) for x in single_values]
+    #    + [Term(multiple_values[x], "ZZ", [x[0], x[1]]) for x in multiple_values],
+    #    constant_coeff=offset,
+    # )
 
-    hamiltonian_qubo = Observable(number_of_qubits,
-                            pauli_terms=
-                            [Term(single_values[x], "Z", [x]) for x in single_values]+
-                            [Term(multiple_values[x], "ZZ", [x[0],x[1]]) for x in multiple_values],
-                            constant_coeff=offset
-                            )
-
-    print(hamiltonian_qubo)
-
+    # print(hamiltonian_qubo)
 
     sampler = neal.SimulatedAnnealingSampler()
     bqm = model.to_bqm()
@@ -76,18 +78,19 @@ def main():
     best_sample = min(decoded_samples, key=lambda x: x.energy)
     pprint(best_sample.sample)
     pprint(best_sample.energy)
-    
-    '''
+
+    """
     bqm = model.to_bqm()
     sa = ExactSolver()
     sampleset = sa.sample(bqm)
     sampleset.to_pandas_dataframe().sort_values("energy")
-    '''
+    """
     # Minimizing Example DEN
-    #minimization_process_cobyla(goal_p, G, num_colors, school, cost_function_den_4pts)
+    # minimization_process_cobyla(goal_p, G, num_colors, school, cost_function_den_4pts)
 
     print("Program End")
     print("----------------------------")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- qubo.py
+++ qubo.py
@@ -1,145 +1,89 @@
+import warnings
 import xhsttparser
 import itertools
-from math import prod
-from functools import reduce
-from pyqubo import And, Or, Xor, Not, OneHotEncInteger, LogEncInteger, NotConst, XorConst, OrConst, AndConst, Constraint
-from pprint import pprint
+from pyqubo import (
+    And,
+    Xor,
+    Not,
+    LogEncInteger,
+    NotConst,
+)
 
-def xhstt_to_qubo(instance):
-    available_times = []
-    #print(f"Instance Times {instance.times}")
-    for time_group in instance.times.time_groups:
-        #print(f"Time_groups {instance.times.time_groups[time_group]}")
-        for time in instance.times.time_groups[time_group]:
-            available_times.append(time)
-            #print(f"Time {time}")
-    
-    existing_timeslots = len(available_times)
-    #print(f"Existing Timeslots {existing_timeslots}")
-    #print(f"Available Times {available_times}")
-    #print("\n---------------------\n")
 
-    all_events_as_variables = []
-    #pprint(f"Instance Events {instance.events}")
-    #print("\n---------------------\n")
-    #pprint("Instance All Events", instance.events.event_groups["gr_AllEvents"])
-    for event in instance.events.event_groups["gr_AllEvents"]:
-        #print(f"Event {event.name}")
-        all_events_as_variables.append(LogEncInteger(event.name.strip(), (0, existing_timeslots-1)))
-        #print("---------------------")
-    
-    #print("All Events", all_events_as_variables)
-    #print("\n---------------------\n")
+class XhsttToQubo:
+    def __init__(self, instance: xhsttparser.Instance) -> None:
+        self.instance = instance
 
-    H = 0
-    for constraint in instance.constraints:
-        if type(constraint) == xhsttparser.AssignTimeConstraint:
-            #H = constraint.weight*sum(all_events_as_variables)
-            #print("Element", constraint.element)
-            #print("Id", constraint.id)
-            #print("Name", constraint.name)
-            #print("Required", constraint.required)
-            #print("Weight", constraint.weight)
-            #print("Cost Function", constraint.cost_function)
-            #print("Applies to", constraint.applies_to)
+        self.available_times = []
+        for time_group in instance.times.time_groups:
+            for time in instance.times.time_groups[time_group]:
+                self.available_times.append(time)
 
-            for group in constraint.applies_to:
-                if group == "event_groups":
-                    pass
-                    #for target in constraint.applies_to[group]:
-                        #print(instance.events.event_groups[target])
-                elif group == "events":
-                    pass
-                elif group == "resource_groups":
-                    pass
-            #print("-------------")
-        
-        elif type(constraint) == xhsttparser.PreferTimesConstraint:
-            #print("Element", constraint.element)
-            #print("Id", constraint.id)
-            #print("Name", constraint.name)
-            #print("Time", constraint.times)
-            #print("Time Group", constraint.time_groups)
-            #print("Required", constraint.required)
-            #print("Weight", constraint.weight)
-            #print("Cost Function", constraint.cost_function)
-            #print("Applies to", constraint.applies_to)
-            
-            for group in constraint.applies_to:
-                if group == "event_groups":
-                    pass
-                elif group == "events":
-                    for target in constraint.applies_to[group]:
-                        #print(f"{target=}")
-                        #print(f"Found {instance.events.event[target].name.strip()}")
-                        for variable in all_events_as_variables:
-                            if instance.events.event[target].name.strip() == variable.label:
-                                for time in constraint.times:
-                                    #print(f"{time=}")
-                                    index = available_times.index(time)
-                                    #print(f"Index {bin(index)}")
-                                    exp = (variable - index)**2
-                                    #print(f"{exp=}")
-                                    #H += 2*constraint.weight*exp
-                elif group == "resource_groups":
-                    pass
-        #print("-------------")
-        
-        elif type(constraint) == xhsttparser.AvoidClashesConstraint:
-            #print("Element", constraint.element)
-            #print("Id", constraint.id)
-            #print("Name", constraint.name)
-            #print("Required", constraint.required)
-            #print("Weight", constraint.weight)
-            #print("Cost Function", constraint.cost_function)
-            #print("Applies to", constraint.applies_to)
+        self.existing_time_slots = len(self.available_times)
 
-            pair_order_list = itertools.combinations(instance.events.event_groups["gr_AllEvents"],2)
+        self.all_events_as_variables = []
+        for event in instance.events.event_groups[
+            "gr_AllEvents"
+        ]:  # TODO Verify if this cover all instances
+            self.all_events_as_variables.append(
+                LogEncInteger(event.name.strip(), (0, self.existing_time_slots - 1))
+            )
 
-            for pair in pair_order_list:
-                resources0 = [res.element.attrib["Reference"] for res in pair[0].resources]
-                resources1 = [res.element.attrib["Reference"] for res in pair[1].resources]
-                conflict = [i for i in resources0 if i in resources1]
-                #conflict = set(resources0+resources1)
-                #print(f"{pair=}")
-                #print("Resources")
-                #print(f"{resources0=}")
-                #print(f"{resources1=}")
-                #print(f"{conflict=}")
-                if conflict:
-                    pair_names = [event.name.strip() for event in pair]
-                    print(f"{pair_names=}\n")
-                    variables = [v for v in all_events_as_variables if v.label in pair_names]
-                    #print(f"{variables=}\n")
-                    #print("Clashing Resource")
-                    #print(variables)
-                    #partials = sum([XorConst(a, b, 0, f'xor_{a}_{b}') for a, b in zip(variables[0].array, variables[1].array)])
-                    #print(f"{partials=}")
-                    # XOR = (A OR B) AND (NOT (A AND B))
-                    #xors = [And(Or(a,b), Not(And(a,b))) for a, b in zip(variables[0].array, variables[1].array)]
-                    xors = [Not(Xor(a,b)) for a, b in zip(variables[0].array, variables[1].array)]
-                    clashs = And(xors[0], xors[1])
-                    #for gate in xors:
-                        #print(f"{gate=}\n")
-                    #factor = len(variables[0].array)
-                    #print(f"{factor=}")
+    def compile(self):
+        h = 0
+        for constraint in self.instance.constraints:
+            if isinstance(constraint, xhsttparser.AssignTimeConstraint):
+                h += self.assign_time_constraint(constraint)
+            # elif isinstance(constraint, xhsttparser.PreferTimesConstraint):
+            #     h += self.prefer_times_constraint(constraint)
+            elif isinstance(constraint, xhsttparser.AvoidClashesConstraint):
+                h += self.avoid_clash_constraint(constraint)
+            else:
+                warnings.warn(f"Constraint {constraint} not supported")
+        return h.compile()
 
-                    exp = NotConst(clashs, 1, f'clash_{pair_names[0]}_{pair_names[1]}')
-                    #exp = AndConst(xors[0], xors[1], 0, f'clash_{pair_names[0]}_{pair_names[1]}')
-                    #exp = (2-sum([Xor(a,b) for a, b in zip(variables[0].array, variables[1].array)]))
-                    #exp = sum([(1-Xor(a,b))**2 for a, b in zip(variables[0].array, variables[1].array)])-2
-                    #exp = reduce(lambda a, b : AndConst(a, b, 1, constraint.name), [(XorConst(a, b, 0, 'xor_partial')) for a, b in zip(variables[0].array, variables[1].array)])
-                    #exp  = AndConst(partials[0], partials[1], 1, constraint.name)
-                    #exp = Constraint((variables[0] - variables[1])**2, f"Clash_{pair_names[0]}_{pair_names[1]}", condition=lambda x: x!=10.0)
-                    #exp = sum([(AndConst(a,b,0, f'xor_{a}_{b}')) for a, b in zip(variables[0].array, variables[1].array)])
-                    #exp = AndConst(a, b, 1, f'and {pair_names[0]} {pair_names[1]}')
-                    #exp = - weight*((variables[0] - variables[1])**2)
-                    print(f"{exp=}\n")
-                    H += 10*constraint.weight*exp
-        
-        #print("-------------")
+    def assign_time_constraint(self, constraint: xhsttparser.AssignTimeConstraint):
+        h = 0
+        return h
 
-    #print("\n---------------------\n")
-    model = H.compile()
-    
-    return model
+    def prefer_times_constraint(self, constraint: xhsttparser.PreferTimesConstraint):
+        h = 0
+        for target in constraint.applies_to["events"]:
+            for variable in self.all_events_as_variables:
+                if self.instance.events.event[target].name.strip() == variable.label:
+                    for time in constraint.times:
+                        index = self.available_times.index(time)
+                        exp = (variable - index) ** 2
+                        h += 2 * constraint.weight * exp
+
+        return h
+
+    def avoid_clash_constraint(self, constraint: xhsttparser.AvoidClashesConstraint):
+        h = 0
+
+        pair_order_list = itertools.combinations(
+            self.instance.events.event_groups["gr_AllEvents"], 2
+        )
+
+        for pair in pair_order_list:
+            resources0 = [res.element.attrib["Reference"] for res in pair[0].resources]
+            resources1 = [res.element.attrib["Reference"] for res in pair[1].resources]
+            conflict = [i for i in resources0 if i in resources1]
+
+            if conflict:
+                pair_names = [event.name.strip() for event in pair]
+                # print(f"{pair_names=}\n")
+                variables = [
+                    v for v in self.all_events_as_variables if v.label in pair_names
+                ]
+
+                xors = [
+                    Not(Xor(a, b))
+                    for a, b in zip(variables[0].array, variables[1].array)
+                ]
+                clashs = And(xors[0], xors[1])
+
+                exp = NotConst(clashs, 1, f"clash_{pair_names[0]}_{pair_names[1]}")
+
+                h += 10 * constraint.weight * exp
+        return h
