069e94f1c00804f05438b5cc01d14de1729f46c6
Evandro Chagas Ribeiro da Rosa <evandro@quantuloop.com>
2023-11-08 20:58:17 -0300

    refac var names

--- qubo.py
+++ qubo.py
@@ -14,20 +14,23 @@ class XhsttToQubo:
     def __init__(self, instance: xhsttparser.Instance) -> None:
         self.instance = instance
 
-        self.available_times = []
-        for time_group in instance.times.time_groups:
-            for time in instance.times.time_groups[time_group]:
-                self.available_times.append(time)
-
-        self.existing_time_slots = len(self.available_times)
-
-        self.all_events_as_variables = []
-        for event in instance.events.event_groups[
-            "gr_AllEvents"
-        ]:  # TODO Verify if this cover all instances
-            self.all_events_as_variables.append(
-                LogEncInteger(event.name.strip(), (0, self.existing_time_slots - 1))
+        self.available_times = {
+            time: index
+            for index, time in enumerate(
+                [
+                    time
+                    for time_group in instance.times.time_groups
+                    for time in instance.times.time_groups[time_group]
+                ]
             )
+        }
+
+        self.num_time_slots = len(self.available_times)
+
+        self.events_as_variables = {
+            event: LogEncInteger(event, (0, self.num_time_slots - 1))
+            for event in instance.events.event.keys()
+        }
 
     def compile(self):
         h = 0
@@ -35,7 +38,7 @@ class XhsttToQubo:
             if isinstance(constraint, xhsttparser.AssignTimeConstraint):
                 h += self.assign_time_constraint(constraint)
             # elif isinstance(constraint, xhsttparser.PreferTimesConstraint):
-            #     h += self.prefer_times_constraint(constraint)
+            #    h += self.prefer_times_constraint(constraint)
             elif isinstance(constraint, xhsttparser.AvoidClashesConstraint):
                 h += self.avoid_clash_constraint(constraint)
             else:
@@ -49,41 +52,29 @@ class XhsttToQubo:
     def prefer_times_constraint(self, constraint: xhsttparser.PreferTimesConstraint):
         h = 0
         for target in constraint.applies_to["events"]:
-            for variable in self.all_events_as_variables:
-                if self.instance.events.event[target].name.strip() == variable.label:
-                    for time in constraint.times:
-                        index = self.available_times.index(time)
-                        exp = (variable - index) ** 2
-                        h += 2 * constraint.weight * exp
-
+            for time in constraint.times:
+                h += (
+                    2
+                    * constraint.weight
+                    * (self.events_as_variables[target] - self.available_times[time])
+                    ** 2
+                )
         return h
 
     def avoid_clash_constraint(self, constraint: xhsttparser.AvoidClashesConstraint):
         h = 0
 
-        pair_order_list = itertools.combinations(
-            self.instance.events.event_groups["gr_AllEvents"], 2
-        )
+        event_pairs = itertools.combinations(self.instance.events.event.items(), 2)
 
-        for pair in pair_order_list:
-            resources0 = [res.element.attrib["Reference"] for res in pair[0].resources]
-            resources1 = [res.element.attrib["Reference"] for res in pair[1].resources]
-            conflict = [i for i in resources0 if i in resources1]
+        for (event_name_a, event_a), (event_name_b, event_b) in event_pairs:
+            resources_a = [res.reference for res in event_a.resources]
+            resources_b = [res.reference for res in event_b.resources]
+            conflict = [i for i in resources_a if i in resources_b]
 
-            if conflict:
-                pair_names = [event.name.strip() for event in pair]
-                # print(f"{pair_names=}\n")
-                variables = [
-                    v for v in self.all_events_as_variables if v.label in pair_names
-                ]
+            if len(conflict) != 0:
+                var_a = self.events_as_variables[event_name_a]
+                var_b = self.events_as_variables[event_name_b]
 
-                xors = [
-                    Not(Xor(a, b))
-                    for a, b in zip(variables[0].array, variables[1].array)
-                ]
-                clashs = And(xors[0], xors[1])
+                h += -((var_a - var_b) ** 2)
 
-                exp = NotConst(clashs, 1, f"clash_{pair_names[0]}_{pair_names[1]}")
-
-                h += 10 * constraint.weight * exp
         return h
