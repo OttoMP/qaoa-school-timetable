9df7ce9f5c83f578908b7001e4eaa111ad1627c0
OttoMP <otto28mai@gmail.com>
2023-11-21 11:08:36 -0300

    OneHot Encoding working fine

--- qubo.py
+++ qubo.py
@@ -12,7 +12,7 @@ from pyqubo import (
 
 
 class XhsttToQubo:
-    def __init__(self, instance: xhsttparser.Instance) -> None:
+    def __init__(self, instance: xhsttparser.Instance, OneHotEnc = True) -> None:
         self.instance = instance
 
         self.available_times = {
@@ -28,18 +28,24 @@ class XhsttToQubo:
 
         self.num_time_slots = len(self.available_times)
 
-        self.events_as_variables = {
-            event: OneHotEncInteger(event, (0, self.num_time_slots - 1), 1)
-            for event in instance.events.event.keys()
-        }
+        if OneHotEnc:
+            self.events_as_variables = {
+                event: OneHotEncInteger(event, (0, self.num_time_slots - 1), 5)
+                for event in instance.events.event.keys()
+            }
+        else:
+            self.events_as_variables = {
+                event: LogEncInteger(event, (0, self.num_time_slots - 1))
+                for event in instance.events.event.keys()
+            }
 
     def compile(self):
         h = 0
         for constraint in self.instance.constraints:
             if isinstance(constraint, xhsttparser.AssignTimeConstraint):
                 h += self.assign_time_constraint(constraint)
-            #elif isinstance(constraint, xhsttparser.PreferTimesConstraint):
-            #    h += self.prefer_times_constraint(constraint)
+            elif isinstance(constraint, xhsttparser.PreferTimesConstraint):
+                h += self.prefer_times_constraint(constraint)
             elif isinstance(constraint, xhsttparser.AvoidClashesConstraint):
                 h += self.avoid_clash_constraint(constraint)
             else:
@@ -48,7 +54,10 @@ class XhsttToQubo:
 
     def assign_time_constraint(self, constraint: xhsttparser.AssignTimeConstraint):
         h = 0
-        #h += sum(self.events_as_variables.values())
+        for key, value in self.events_as_variables.items():
+            h += 1 - sum(value.array)
+            for a,b in itertools.combinations(value.array, 2):
+                h += 2*a*b
         return h
 
     def prefer_times_constraint(self, constraint: xhsttparser.PreferTimesConstraint):
@@ -77,7 +86,10 @@ class XhsttToQubo:
                 print(f"{event_name_a=} {event_name_b=}")
                 var_a = self.events_as_variables[event_name_a]
                 var_b = self.events_as_variables[event_name_b]
-
-                h += -((var_a - var_b) ** 2)
+                
+                for bit_a, bit_b in zip(var_a.array, var_b.array):
+                    h += 5*constraint.weight*(bit_a*bit_b)
+                
+                #h += -((var_a - var_b) ** 2)
 
         return h
